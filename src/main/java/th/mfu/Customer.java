3252e832099e342e21ba96845934a4c675c72197
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-08-10 11:30:37 +0700

    update

--- src/main/java/th/mfu/Customer.java
+++ src/main/java/th/mfu/Customer.java
@@ -3,15 +3,23 @@ package th.mfu;
 public class Customer {
     private String name;
     private String email;
+    private String address;
     private String phone;
 
     public Customer() {
     }
 
-    public Customer(String name, String email, String phone) {
+    public Customer(String name, String email, String address, String phone) {
         this.name = name;
         this.email = email;
+        this.address = address;
         this.phone = phone;
+    }
+
+    public Customer(String name, String email, String address) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
     }
 
     public Customer(String name, String email) {
@@ -22,7 +30,6 @@ public class Customer {
     public String getName() {
         return name;
     }
-
 
     public void setName(String name) {
         this.name = name;
@@ -36,6 +43,13 @@ public class Customer {
         this.email = email;
     }
 
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getPhone() {
         return this.phone;
